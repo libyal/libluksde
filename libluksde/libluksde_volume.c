82010e985f91163801c2315ec803c5aa2625e854
Joachim Metz <joachim.metz@gmail.com>
2014-10-17 08:03:01 +0200

    Changes for deployment

--- libluksde/libluksde_volume.c
+++ libluksde/libluksde_volume.c
@@ -1351,7 +1351,7 @@ int libluksde_volume_open_read(
 		     NULL,
 		     (int (*)(intptr_t *, intptr_t *, libfdata_vector_t *, libfcache_cache_t *, int, int, off64_t, size64_t, uint32_t, uint8_t, libcerror_error_t **)) &libluksde_io_handle_read_sector,
 		     NULL,
-		     LIBFDATA_FLAG_DATA_HANDLE_NON_MANAGED,
+		     LIBFDATA_DATA_HANDLE_FLAG_NON_MANAGED,
 		     error ) != 1 )
 		{
 			libcerror_error_set(
