1b258ed4bc08bb547ccfe141f073b365bae2cd02
Joachim Metz <joachim.metz@gmail.com>
2015-01-06 08:45:47 +0100

    Worked on Python bindings.

--- pyluksde/pyluksde.c
+++ pyluksde/pyluksde.c
@@ -461,6 +461,14 @@ PyMODINIT_FUNC initpyluksde(
 	PyTypeObject *volume_type_object                      = NULL;
 	PyGILState_STATE gil_state                            = 0;
 
+#if defined( HAVE_DEBUG_OUTPUT )
+	libluksde_notify_set_stream(
+	 stderr,
+	 NULL );
+	libluksde_notify_set_verbose(
+	 1 );
+#endif
+
 	/* Create the module
 	 * This function must be called before grabbing the GIL
 	 * otherwise the module will segfault on a version mismatch
