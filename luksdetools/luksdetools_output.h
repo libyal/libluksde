ea45e1ba2ce30e98a50c0f88869d5bccac0f380c
Joachim Metz <joachim.metz@gmail.com>
2018-01-30 07:04:30 +0100

    Applied updates

--- luksdetools/luksdetools_output.h
+++ luksdetools/luksdetools_output.h
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the luksdetools
  *
- * Copyright (C) 2013-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2013-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
