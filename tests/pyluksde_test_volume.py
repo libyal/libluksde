d94261a84a2fb73dcc7d264144b806060a0e2f1c
Joachim Metz <joachim.metz@gmail.com>
2019-04-27 19:11:40 +0200

    Applied updates

--- tests/pyluksde_test_volume.py
+++ tests/pyluksde_test_volume.py
@@ -280,12 +280,17 @@ if __name__ == "__main__":
   argument_parser = argparse.ArgumentParser()
 
   argument_parser.add_argument(
+      "-p", "--password", dest="password", action="store", default=None,
+      type=str, help="password to unlock the source file.")
+
+  argument_parser.add_argument(
       "source", nargs="?", action="store", metavar="PATH",
-      default=None, help="The path of the source file.")
+      default=None, help="path of the source file.")
 
   options, unknown_options = argument_parser.parse_known_args()
   unknown_options.insert(0, sys.argv[0])
 
+  setattr(unittest, "password", options.password)
   setattr(unittest, "source", options.source)
 
   unittest.main(argv=unknown_options, verbosity=2)
