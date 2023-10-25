9120d432e7c6da7410666d641748f7bc7c6d42e2
Joachim Metz <joachim.metz@gmail.com>
2023-10-25 06:32:42 +0200

    Changes for CI tests

--- synctestdata.sh
+++ synctestdata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script that synchronizes the local test data
 #
-# Version: 20161009
+# Version: 20231001
 
 TEST_SET="public";
 TEST_INPUT_DIRECTORY="tests/input";
@@ -12,14 +12,19 @@ mkdir -p "${TEST_INPUT_DIRECTORY}/.libluksde/${TEST_SET}";
 mkdir -p "${TEST_INPUT_DIRECTORY}/.pyluksde/${TEST_SET}";
 mkdir -p "${TEST_INPUT_DIRECTORY}/.luksdeinfo/${TEST_SET}";
 
+cat > test_data_options << EOT
+# libyal test data options
+password=luksde-TEST
+EOT
+
 for TEST_FILE in ${TEST_FILES};
 do
 	URL="https://github.com/log2timeline/dfvfs/blob/main/test_data/${TEST_FILE}?raw=true";
 
 	curl -L -o "${TEST_INPUT_DIRECTORY}/${TEST_SET}/${TEST_FILE}" ${URL};
 
-	echo "-pluksde-TEST" > "${TEST_INPUT_DIRECTORY}/.libluksde/${TEST_SET}/${TEST_FILE}.password";
-	echo "-pluksde-TEST" > "${TEST_INPUT_DIRECTORY}/.pyluksde/${TEST_SET}/${TEST_FILE}.password";
-	echo "-pluksde-TEST" > "${TEST_INPUT_DIRECTORY}/.luksdeinfo/${TEST_SET}/${TEST_FILE}.password";
+	cp test_data_options "${TEST_INPUT_DIRECTORY}/.libluksde/${TEST_SET}/${TEST_FILE}.password";
+	cp test_data_options "${TEST_INPUT_DIRECTORY}/.pyluksde/${TEST_SET}/${TEST_FILE}.password";
+	cp test_data_options "${TEST_INPUT_DIRECTORY}/.luksdeinfo/${TEST_SET}/${TEST_FILE}.password";
 done
 
