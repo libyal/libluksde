0e02b5750068f08217460a75ce2bc250cac96bc1
Joachim Metz <joachim.metz@gmail.com>
2026-05-15 20:14:32 +0200

    Moved setuptools configuration into pyproject.toml

--- luksdetools/luksdemount.c
+++ luksdetools/luksdemount.c
@@ -1,7 +1,7 @@
 /*
  * Mounts a Linux Unified Key Setup (LUKS) Disk Encrypted volume.
  *
- * Copyright (C) 2013-2025, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2013-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
