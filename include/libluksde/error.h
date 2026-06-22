7cafecf8c998ec37cd72c4db10a679b8725f9713
Joachim Metz <joachim.metz@gmail.com>
2026-06-22 07:41:56 +0200

    Worked on tests and changes for CI tests

--- include/libluksde/error.h
+++ include/libluksde/error.h
@@ -82,7 +82,7 @@ enum LIBLUKSDE_ARGUMENT_ERROR
 	 */
 	LIBLUKSDE_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBLUKSDE_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBLUKSDE_OUTPUT_ERROR
 {
 	LIBLUKSDE_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBLUKSDE_OUTPUT_ERROR_INSUFFICIENT_SPACE	= 1
 };
--- synctestdata.ps1
+++ synctestdata.ps1
@@ -38,3 +38,5 @@ ForEach ($TestFile in ${TestFiles} -split " ")
 	Copy-Item test_data_options -Destination "${TestInputDirectory}\.luksdeinfo\${TestSet}\${TestFile}.password"
 }
 
+Remove-Item -Force -Path test_data_options
+
